3d11252b9f5fae84f99f33d7b50690b6f3b27c0c
BWPanda <peter@panda.id.au>
2020-03-14 21:47:46 +0200

    Fix require_once path.

--- .tugboat/auto-login.sh
+++ .tugboat/auto-login.sh
@@ -4,7 +4,7 @@
  * Automatically login to a newly-installed Backdrop site.
  */
 
-require_once '../core/includes/install.core.inc';
+require_once './core/includes/install.core.inc';
 global $user;
 $user = user_load(1);
 user_login_finalize();
