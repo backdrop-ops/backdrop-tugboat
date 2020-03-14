1fc91c6b2bd01482e98ef16ec58c7c1dc9b620c4
BWPanda <peter@panda.id.au>
2020-03-14 22:00:22 +0200

    Bootstrap Backdrop.

--- .tugboat/auto-login.sh
+++ .tugboat/auto-login.sh
@@ -4,7 +4,12 @@
  * Automatically login to a newly-installed Backdrop site.
  */
 
-require_once './core/modules/user/user.module';
+// Bootstrap Backdrop.
+define('BACKDROP_ROOT', '.');
+require_once BACKDROP_ROOT . '/core/includes/bootstrap.inc';
+backdrop_bootstrap(BACKDROP_BOOTSTRAP_FULL);
+
+// Login as user 1.
 global $user;
 $user = user_load(1);
 user_login_finalize();
