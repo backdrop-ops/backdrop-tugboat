1ce78c1d79d5cefef6bdcb0239aa4e5405851a72
BWPanda <peter@panda.id.au>
2020-03-15 10:45:44 +0200

    Generate login URL for user 1.

--- .tugboat/auto-login.sh
+++ .tugboat/auto-login.sh
@@ -9,7 +9,5 @@ define('BACKDROP_ROOT', '.');
 require_once BACKDROP_ROOT . '/core/includes/bootstrap.inc';
 backdrop_bootstrap(BACKDROP_BOOTSTRAP_FULL);
 
-// Login as user 1.
-global $user;
-$user = user_load(1);
-print user_pass_reset_url($user);
+// Generate login URL for user 1.
+print user_pass_reset_url(user_load(1)) . '/login';
