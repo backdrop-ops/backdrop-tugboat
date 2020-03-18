731e14c7d248922c86fc880dbf0109856d24bad5
BWPanda <peter@panda.id.au>
2020-03-18 15:15:30 +0200

    Prepend login URL for easier retrieval.

--- .tugboat/auto-login.sh
+++ .tugboat/auto-login.sh
@@ -10,4 +10,10 @@ require_once BACKDROP_ROOT . '/core/includes/bootstrap.inc';
 backdrop_bootstrap(BACKDROP_BOOTSTRAP_FULL);
 
 // Generate login URL for user 1.
-print user_pass_reset_url(user_load(1)) . '/login';
+$url = user_pass_reset_url(user_load(1)) . '/login';
+
+// Get just the relative URL.
+$url = substr($url, strrpos($url, '/user/reset/'));
+
+// Output the URL (prepended so it can be easily found in the log).
+print "Login URL: $url\n";
