fd327b55d541eb2cde1ab71bd3545378d4b69ca7
BWPanda <peter@panda.id.au>
2020-03-30 09:12:55 +0300

    Issue #10: Remove Backdrop code from repo.

--- .tugboat/settings.local.php
+++ .tugboat/settings.local.php
@@ -1,7 +1,13 @@
 <?php
+// Database.
+$database = 'mysql://tugboat:tugboat@mariadb/tugboat';
+$database_charset = 'utf8mb4';
 
-$database = 'mysql://tugboat:tugboat@mysql/backdrop';
-$database_prefix = '';
-#$database_charset = 'utf8mb4';
+// Config.
+$config_directories['active'] = 'files/config/active';
+$config_directories['staging'] = 'files/config/staging';
 
+// Trusted hosts.
 $settings['trusted_host_patterns'] = array('^.+\.tugboat\.qa$');
+
+// Miscellaneous.
